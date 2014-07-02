ee946fc7b11e984cbf4dc3ec4a9b47e66ce68aa2
sebastien <lorber.sebastien@gmail.com>
2014-07-02 14:51:33 +0200

    Fix less compiling

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -1,10 +1,16 @@
 var gulp = require('gulp');
 var recess = require('gulp-recess');
 var less = require('gulp-less');
+var handleErrors = require('../util/handleErrors');
+
 
 gulp.task('styles', function () {
+
     return gulp.src('./src/css/app.less')
-        // .pipe(recess())
+        .pipe(recess())
+        .on('error', handleErrors) // TODO maybe we should still compile less files even if Recess is not happy
         .pipe(less())
+        .on('error', handleErrors)
         .pipe(gulp.dest('./www'));
+
 });
